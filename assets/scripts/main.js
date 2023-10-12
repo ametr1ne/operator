f7497ff10855e2ccc2e6a97d58cb0f80dba206ed
ametr1ne <nikon.filanovsky@gmail.com>
2023-10-12 12:24:44 +0400

    mobile

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,13 +2,27 @@ const kf = 517 / 1920
 
 let edgePadding = document.body.clientWidth * kf
 
- let options = {
-     container: '.comments-slider',
-     items: 1,
-     controlsContainer: ".comments-controls",
-     mouseDrag: true,
-     edgePadding: edgePadding
- }
+let options = {
+    container: '.comments-slider',
+    controlsContainer: ".comments-controls",
+    mouseDrag: true,
+    items: 3,
+    responsive: {
+        320: {
+            items: 1,
+            fixedWidth: 284,
+        },
+        768: {
+            edgePadding: edgePadding,
+            items: 1,
+            fixedWidth: false,
+        },
+        1024: {
+            edgePadding: 0,
+            items: 3
+        }
+    }
+}
 
 let slider = tns(options);
 
@@ -18,20 +32,47 @@ window.addEventListener("resize", function () {
 
     options = {
         container: '.comments-slider',
-        items: 1,
         controlsContainer: ".comments-controls",
         mouseDrag: true,
-        edgePadding: edgePadding
+        responsive: {
+            320: {
+                items: 1,
+                fixedWidth: 284,
+            },
+            768: {
+                edgePadding: edgePadding,
+                items: 1,
+                fixedWidth: false,
+            },
+            1024: {
+                edgePadding: 0,
+                items: 3
+            }
+        }
     }
 
     slider.destroy()
 
     slider = tns(options)
+
+    burgerHandler()
 })
 
-if (document.body.clientWidth <= 1024) {
-    document.querySelector(".header-burger").addEventListener("click", function () {
-        document.body.classList.toggle("opened")
-    })
+burgerHandler()
+
+function burgerHandler() {
+    if (document.body.clientWidth <= 1024) {
+        document.querySelector(".header-burger").addEventListener("click", function () {
+            document.body.classList.toggle("opened")
+        })
+    }
 }
+
+const nav_links = document.querySelectorAll(".header-menu__link")
+
+nav_links.forEach(function (item, index) {
+    item.addEventListener("click", function () {
+        document.body.classList.remove("opened")
+    })
+})
 
